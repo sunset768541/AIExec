a1315f066f1760b678c4f7ca91d0681b50516307
wangbaohui <13802885224@139.com>
2020-04-23 17:03:12 +0800

    添加处理

--- Te.py
+++ Te.py
@@ -42,18 +42,24 @@ def neturalTest():
   x_image = tf.reshape(x, [-1,28,28,1])
   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
   h_pool1 = max_pool_2x2(h_conv1)
+  print(h_pool1.shape)
 
   W_conv2 = weight_variable([5, 5, 32, 64])
   b_conv2 = bias_variable([64])
 
   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
   h_pool2 = max_pool_2x2(h_conv2)
+  print(h_pool2.shape)
 
   W_fc1 = weight_variable([7 * 7 * 64, 1024])
   b_fc1 = bias_variable([1024])
 
   h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+  print(h_pool2_flat.shape)
+
   h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+  print(h_fc1.shape)
+
   rate = 1 - tf.compat.v1.placeholder("float")
   h_fc1_drop = tf.nn.dropout(h_fc1, rate)
   W_fc2 = weight_variable([1024, 10])
